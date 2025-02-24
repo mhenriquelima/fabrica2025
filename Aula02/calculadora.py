f1b3a4348dedcfcc88d174c07346c353be03b3b5
mhenriquelima <mhenriquedelima1@gmail.com>
2025-02-24 14:23:25 -0300

    upd

--- Aula02/calculadora.py
+++ Aula02/calculadora.py
@@ -2,6 +2,7 @@ historico = []
 class cal:
     def adcionar(n1,n2):
         result = n1 + n2
+        
         return result
     def subtrair(n1,n2):
         result = n1 - n2
@@ -16,10 +17,11 @@ class cal:
             result = n1 / n2
         return result
     def mostrar_historico():
-        print(historico)
+        print("Seu histórico: {}".format(historico))
         
 while True:
-    calculo = int(input("""ql o calculo 
+    calculo = int(input("""
+    ql o calculo 
     [0] - soma 
     [1] - subtracao 
     [2] - multiplicacao 
@@ -29,14 +31,14 @@ while True:
     > """))
 
     if calculo <= 3:
-        n1 = int(input("n1 > "))
-        n2 = int(input("n2 > "))
-        calculoList = [cal.adcionar(n1,n2),cal.subtrair(n1,n2),cal.multiplicar(n1,n2),cal.dividir(n1,n2),cal.mostrar_historico()]
+        n1 = float(input("n1 > "))
+        n2 = float(input("n2 > "))
+        calculoList = [cal.adcionar(n1,n2),cal.subtrair(n1,n2),cal.multiplicar(n1,n2),cal.dividir(n1,n2),cal.mostrar_historico(),]
         print(calculoList[calculo])
         historico.append(calculoList[calculo])
-    elif 4 == calculo:
+    elif calculo == 4:
         cal.mostrar_historico()
-    elif 5 == calculo:
+    elif calculo == 5:
         historico.clear()
     else:
-        print("escolha uma opcao valida")
+        print("escolha uma opcao valida".upper())
