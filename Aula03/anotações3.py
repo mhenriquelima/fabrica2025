614a02843b7e150b949c2ecd29a80bae60a10c5a
mhenriquelima <mhenriquedelima1@gmail.com>
2025-02-24 17:34:41 -0300

    Create anotações3.py

---
+++ Aula03/anotações3.py
@@ -0,0 +1,17 @@
+#try except
+    #qnd erro eh possivel
+def dividir(n1,n2):
+    try:
+        result = n1/n2
+    except ZeroDivisionError:
+        return "pd dividir por 0 nao man"
+
+#stacktrace
+    #qnd der erro pra achar
+
+#constantes
+    #LETRAS MAIUSCULAS
+
+#Django
+    #
+    
