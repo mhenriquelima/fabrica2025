c63cc370b55ea4c5b88b0028dd96e3332d84cc4f
mhenriquelima <mhenriquedelima1@gmail.com>
2025-02-24 13:35:24 -0300

    upd

--- print_foda.py
+++ print_foda.py
@@ -1,2 +1,2 @@
 _frase = str(input("frase > ").strip().lower())
-print("su frase tem {} a's, o primeiro apareceu na posição {} e o ultimo {}".format(_frase.count("a"), _frase.find("a"), _frase.rfind("a")))
+print("sua frase tem {} a's, o primeiro apareceu na posição {} e o ultimo {}".format(_frase.count("a"), _frase.find("a"), _frase.rfind("a")))
