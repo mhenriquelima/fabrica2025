defa130ebf33c73a1e576da4e95c91c175628871
mhenriquelima <mhenriquedelima1@gmail.com>
2025-02-24 16:31:47 -0300

    upd calculadora

--- Aula02/calculadora.py
+++ Aula02/calculadora.py
@@ -1,3 +1,4 @@
+print("se usar letra vai toma break")
 historico = []
 class cal:
     def adcionar(n1,n2):
@@ -18,9 +19,9 @@ class cal:
         return result
     def mostrar_historico():
         print("Seu histórico: {}".format(historico))
-        
-while True:
-    calculo = int(input("""
+
+while Is_running == True:
+    calculo = input("""
     ql o calculo 
     [0] - soma 
     [1] - subtracao 
@@ -28,17 +29,25 @@ while True:
     [3] - divisao
     [4] - mostrar historico
     [5] - limpar
-    > """))
-
-    if calculo <= 3:
+    > """)
+    try:
+        result = int(calculo) + 1
+    except TypeError:
+        print("reinicie")
+        break
+    except ValueError:
+        print("reinicie")
+        break
+    
+    if int(calculo) <= 3:
         n1 = float(input("n1 > "))
         n2 = float(input("n2 > "))
         calculoList = [cal.adcionar(n1,n2),cal.subtrair(n1,n2),cal.multiplicar(n1,n2),cal.dividir(n1,n2),cal.mostrar_historico(),]
-        print(calculoList[calculo])
-        historico.append(calculoList[calculo])
-    elif calculo == 4:
+        print(calculoList[int(calculo)])
+        historico.append(calculoList[int(calculo)])
+    elif int(calculo) == 4:
         cal.mostrar_historico()
-    elif calculo == 5:
+    elif int(calculo) == 5:
         historico.clear()
     else:
-        print("escolha uma opcao valida".upper())
+        print("ESCOLHA UMA OPCAO VALIDA")
